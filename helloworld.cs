eee4aefce6bdde7cafde1a95ee1facccd0827c45
Adarshpatel07 <adarshpatel9329@gmail.com>
2025-05-07 14:30:45 +0530

    commit for add helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,14 @@
+
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+namespace HelloWorldProgram
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Try programiz.pro");
+        }
+    }
+}
