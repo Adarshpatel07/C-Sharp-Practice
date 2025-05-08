8cede662a69a6078fb1d64f8f2d23772bdaee089
Adarshpatel07 <adarshpatel9329@gmail.com>
2025-05-08 14:26:45 +0530

    Division of two number's

---
+++ basics/DivideoftwoNumber.cs
@@ -0,0 +1,17 @@
+//Write a C# Sharp program to print the result of dividing two numbers.
+
+using System;
+
+namespace Divide
+{
+    public class DivideTwoNumber
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 50;
+            int num2 = 5;
+            
+            Console.WriteLine(num1/num2);
+        }
+    }
+}
