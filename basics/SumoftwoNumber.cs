2471364dd2bf5d6fc1d3ef058d79e4f4bfffaca6
Adarshpatel07 <adarshpatel9329@gmail.com>
2025-05-08 14:19:45 +0530

    Program to calculate the sum of two number's

---
+++ basics/SumoftwoNumber.cs
@@ -0,0 +1,15 @@
+//Write a program to calculate the sume of two number in c#
+
+using System;
+namespace CalculateSume
+{
+    class SumOfTwoNumber
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
